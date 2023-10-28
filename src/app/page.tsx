5b07571bbd0e084b1fa07f7073091cf010d81b71
albinbicaj <bicajalbin@gmail.com>
2023-10-28 10:09:53 +0200

    my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,247 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [time, setTime] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [groupGuid, setGroupGuid] = useState("");
+  const [links, setLinks] = useState(() => {
+    const storedLinks = localStorage.getItem("shortenedLinks");
+    return storedLinks ? JSON.parse(storedLinks) : [];
+  });
+  const [timers, setTimers] = useState<{ [key: number]: NodeJS.Timeout }>({});
+  const [error, setError] = useState("");
+
+  const startTimer = (linkId: number, expirationTime: number) => {
+    const timerId = setTimeout(() => {
+      handleDeleteLink(linkId);
+    }, expirationTime * 1000 * 60);
+    setTimers((prevTimers) => ({
+      ...prevTimers,
+      [linkId]: timerId,
+    }));
+  };
+
+  useEffect(() => {
+    links.forEach((link: any) => {
+      if (link.expirationTime) {
+        startTimer(link.id, link.expirationTime);
+      }
+    });
+  }, [links]);
+
+  const handleDeleteLink = (linkId: any) => {
+    clearTimeout(timers[linkId]); // Clear the timer for the link
+    const updatedLinks = links.filter((link: any) => link.id !== linkId);
+    setLinks(updatedLinks);
+    localStorage.setItem("shortenedLinks", JSON.stringify(updatedLinks));
+  };
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setTime(event.target.value);
+    setError("");
+  };
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      if (time && inputValue) {
+        const requestBody = {
+          long_url: inputValue,
+          domain: "bit.ly",
+          group_guid: "",
+        };
+
+        const config = {
+          headers: {
+            Authorization: "Bearer 80dfa7dff5a9650d00a31139ca65118c95fe1d0e",
+            "Content-Type": "application/json",
+          },
+        };
+
+        const res = await axios.post(
+          "https://api-ssl.bitly.com/v4/shorten",
+          requestBody,
+          config
+        );
+
+        const newLink = {
+          id: Date.now(),
+          link: res.data.link,
+          expirationTime: time,
+        };
+
+        setLinks([...links, newLink]);
+
+        startTimer(newLink.id, Number(newLink.expirationTime));
+
+        localStorage.setItem(
+          "shortenedLinks",
+          JSON.stringify([...links, newLink])
+        );
+
+        setInputValue("");
+        setTime("");
+      } else {
+        setError("Please select an expiration time and provide a URL.");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box>
+      <Grid container>
+        <Grid
+          item
+          xs={3.5}
+          sx={{
+            backgroundColor: "#EFEFEF",
+            height: "600px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "25px",
+            pt: "50px",
+          }}
+        >
+          <Box
+            component="img"
+            src="logo.png"
+            sx={{ width: { xs: "100%", sm: "100px" } }}
+          />
+          <Box>
+            <Typography sx={{ fontSize: "18px", fontWeight: 600 }}>
+              My shortened URLs
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "start",
+              flexDirection: "column",
+              gap: "10px",
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+            {links.map((link: any) => (
+              <Box
+                key={link.id}
+                sx={{ display: "flex", alignItems: "center", gap: "15px" }}
+              >
+                <a
+                  href={link.link}
+                  style={{ color: "#1b9de5" }}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {link.link}
+                </a>
+                <DeleteIcon
+                  onClick={() => handleDeleteLink(link.id)}
+                  style={{ cursor: "pointer" }}
+                />
+              </Box>
+            ))}
+          </Box>
+        </Grid>
+        <Grid
+          xs={8.5}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+            gap: "25px",
+            pt: "50px",
+            pl: "150px",
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          <Box>
+            <Typography sx={{ fontSize: "28px", fontWeight: 600 }}>
+              URL Shortener
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+            <Box sx={{ display: "flex", alignItems: "baseline", gap: "10px" }}>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <TextField
+                  sx={{ width: "400px" }}
+                  id="outlined-url-input"
+                  label="Paste the URL to be shortened"
+                  type="url"
+                  autoComplete="current-url"
+                  size="small"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                {error && (
+                  <Typography sx={{ color: "red", fontSize: "12px" }}>
+                    {error}
+                  </Typography>
+                )}
+              </Box>
+              <FormControl sx={{ m: 1, minWidth: 210 }} size="small">
+                <InputLabel id="demo-select-small-label">
+                  Add expiration date
+                </InputLabel>
+                <Select
+                  labelId="demo-select-small-label"
+                  id="demo-select-small"
+                  value={time}
+                  label="Add expiration date"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={1}>1 minute</MenuItem>
+                  <MenuItem value={5}>5 minutes</MenuItem>
+                  <MenuItem value={30}>30 minute</MenuItem>
+                  <MenuItem value={60}>1 hour</MenuItem>
+                  <MenuItem value={300}>5 hour</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "start",
+              }}
+            >
+              <Button
+                onClick={fetchData}
+                sx={{
+                  backgroundColor: "#92278F",
+                  color: "#fff",
+                  p: "8px 25px",
+                  "&:hover": {
+                    backgroundColor: "#92278F",
+                  },
+                  "&:active": {
+                    backgroundColor: "#92278F",
+                  },
+                }}
+              >
+                Shorten URL
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
